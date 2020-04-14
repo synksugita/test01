a117d819266672fa1cf0cb2a5508f6f37ca6c86d
sugita <sugita@synk.co.jp>
2020-04-14 12:24:05 +0900

    課題ゲーム制作

--- game.js
+++ game.js
@@ -1,34 +1,14 @@
 //definition
 //--------------------------------
 
-var Root = function(data){
+var ObjectContainer = function(){
 	Pixim.Container.call(this);
-	this.data = data;
 
-	this.InitialTime = 30;
-	this.timeOld = this.InitialTime;
-	this.bufTime = 0;
+	this.data = 0;
 	this.score = 0;
-
-	this.objectContainer = new PIXI.Container();
-	this.addChild(this.objectContainer);
-	this.header = new PIXI.Container();
-	this.addChild(this.header);
-
-	this.textTime = new PIXI.Text('TIME',{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'});
-	this.textTime.x = app.view.width/2;
-	this.header.addChild(this.textTime);
-	this.textScore = new PIXI.Text('SCORE',{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'});
-	this.textScore.x = app.view.width/4*3;
-	this.header.addChild(this.textScore);
-
-	this.start();
 }
-Root.prototype = new Pixim.Container();
-Root.prototype.start = function(){
-	this.task.on('anim',function(e){this.gameloop(e)});
-}
-Root.prototype.createObject = function(){
+ObjectContainer.prototype = new Pixim.Container();
+ObjectContainer.prototype.createObject = function(){
 	var kind = parseInt(Math.random() * 10);
 	var sprite = new PIXI.Sprite(this.data.resources.images["number" + kind]);
 	sprite.kind = kind + 1;
@@ -38,55 +18,83 @@ Root.prototype.createObject = function(){
 	sprite.x = Math.random() * (app.view.width - sprite.width);
 	sprite.y = 0;// - sprite.height;
 	sprite.interactive = true;
+	sprite.parent = this;
 
-	var _click = 'pointerdown';
-	sprite.on(_click,this.clickEvent);
+	sprite.on('pointerdown',this.clickEvent);
 
-	this.objectContainer.addChild(sprite);
+	this.addChild(sprite);
 }
-Root.prototype.releaseObject = function(number){
-	this.objectContainer.removeChildAt(number);
+ObjectContainer.prototype.releaseObject = function(number){
+	this.removeChildAt(number);
 }
-Root.prototype.clickEvent = function(){
-	this.parent.parent.score += this.moveValue;
-	this.texture = 0;
+ObjectContainer.prototype.clickEvent = function(){
+	this.parent.score += this.moveValue;
 	this.kind = 0;
+	this.texture = 0;
+}
+
+var Header = function(){
+	Pixim.Container.call(this);
+
+	this.InitialTime = 30;
+	this.timeOld = this.InitialTime;
+	this.bufTime = 0;
+
+	this.textTime = new PIXI.Text('TIME',{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'});
+	this.textTime.x = app.view.width/2;
+	this.addChild(this.textTime);
+	this.textScore = new PIXI.Text('SCORE',{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'});
+	this.textScore.x = app.view.width/4*3;
+	this.addChild(this.textScore);
+}
+Header.prototype = new Pixim.Container();
+
+var Root = function(data){
+	Pixim.Container.call(this);
+
+	this.objectContainer = new ObjectContainer();
+	this.addChild(this.objectContainer);
+	this.header = new Header();
+	this.addChild(this.header);
+
+	this.objectContainer.data = data;
+
+	this.start();
+}
+Root.prototype = new Pixim.Container();
+Root.prototype.start = function(){
+	this.task.on('anim',function(e){this.gameloop(e)});
 }
 Root.prototype.gameloop = function(e){
-	this.bufTime += e.delta;
-	var passedtime = parseInt(this.InitialTime - this.bufTime);
-	//var passedtime = parseInt(this.InitialTime - this.watch.getPassedTime()/1000);
+	this.header.bufTime += e.delta / 60;
+	var passedtime = parseInt(this.header.InitialTime - this.header.bufTime);
 
-	this.textTime.text = "TIME : " + passedtime;
-	this.textScore.text = "SCORE : " + this.score;
+	this.header.textTime.text = "TIME : " + passedtime;
+	this.header.textScore.text = "SCORE : " + this.objectContainer.score;
 
 	if(passedtime > 0){
 
-		if(passedtime != this.timeOld){
-			this.createObject(this.data);
+		if(passedtime != this.header.timeOld){
+			this.objectContainer.createObject();
 		}
-		this.timeOld = passedtime;
+		this.header.timeOld = passedtime;
 
-		//コンテナの中身をインデックス指定で１つずつ削除する場合、
-		//最後尾からアクセスして削除していけば
-		//問題なく動作すると思いました。
 		for(var i = this.objectContainer.children.length - 1; i >= 0; i--){
 			var obj = this.objectContainer.children[i];
 			if(obj.y > app.view.height){
-				this.releaseObject(i);
+				this.objectContainer.releaseObject(i);
 				continue;
 			}
 			obj.y += obj.moveValue;
 		}
 	}
 	else{
-		this.textTime.text = "";
-		this.textScore.text = "SCORE : " + this.score;
-		this.textScore.x = app.view.width/2;
-		this.textScore.y = app.view.height/2;
+		this.header.textTime.text = "";
+		this.header.textScore.text = "SCORE : " + this.objectContainer.score;
+		this.header.textScore.x = app.view.width/2;
+		this.header.textScore.y = app.view.height/2;
 
 		this.objectContainer.removeChildren();
-		//this.task.off('anim',this.gameloop);
 		this.task.destroy();
 		console.log('end');
 	}
