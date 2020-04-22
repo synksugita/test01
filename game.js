7e87d979b4815e1d8b8c0dc64eba009aab387e1b
sugita <sugita@synk.co.jp>
2020-04-22 18:48:55 +0900

    課題マインスイーパー

--- game.js
+++ game.js
@@ -1,182 +1,222 @@
-//definition
-//--------------------------------
-
-var FPS = 60;
-var InitialTime = 30;
-
-var ObjectContainer = function($, root) {
+var Header = function($){
 	Pixim.Container.call(this);
 
-	this.data = $;
-	this.root = root;
+	this.$ = $;
 
-	//タイマー用変数
-	this.bufTime = 0;//時間差の記録用
-
-	this.on('selectObj', function(obj, point){
-		this.selectObj(obj, point);
-	});
+	this.textTime = this.addChild(new PIXI.Text('TIME',{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'}));
+	this.textTime.x = this.$.width/2;
+	this.textTime.y = 100;
 }
 
-ObjectContainer.prototype = Object.create(Pixim.Container.prototype);
+Header.prototype = Object.create(Pixim.Container.prototype);
 
-ObjectContainer.prototype.update = function(deltaTime) {
-	this.bufTime += deltaTime;
+Header.prototype.updateTime = function(time){
+	this.textTime.text = ('TIME : ' + time);
+}
 
-	//１秒タイマー
-	if (this.bufTime >= FPS){
-		//オブジェクトを生成
-		this.createObject();
 
-		this.bufTime -= FPS;
+var Block = function(X, Y, texture){
+	Pixim.Container.call(this);
+
+	this.sprite = this.addChild(new PIXI.Sprite(texture));
+
+	this.sprite.width = 10;
+	this.sprite.height = 10;
+	this.sprite.x = X * this.sprite.width;
+	this.sprite.y = Y * this.sprite.height;
+
+	this.posX = X;
+	this.posY = Y;
+
+	this.isOpen = false;
+	this.isMine = false;
+	this.isFlag = false;
+	this.number = 0;
+
+	this.on('pointerdown', function(){
+		this.emit('open', this);
+	});
+	this.interactive = true;
+}
+
+Block.prototype = Object.create(Pixim.Container.prototype);
+
+
+var Board = function($){
+	Pixim.Container.call(this);
+
+	this.$ = $;
+
+	//this.blockContainer = this.addChild(new Pixim.Container());
+
+	this.x = 100;
+	this.y = 100;
+	this.sizeX = 0;
+	this.sizeY = 0;
+}
+
+Board.prototype = Object.create(Pixim.Container.prototype);
+
+Board.prototype.initialize = function(nMine){
+	//if(this.sizeX <= 0 && this.sizeY <= 0){return;}
+	if(this.children.length <= 0){return;}
+
+	this.clearBlocks();
+	this.putMines(nMine);
+}
+
+Board.prototype.clearBlocks = function(){
+	var nBlock = this.sizeX * this.sizeY;
+	for(var i = 0; i < nBlock; i++){
+		var block = this.children[i];
+		block.isOpen = false;
+		block.isMine = false;
+		block.isFlag = false;
+		block.number = 0;
 	}
+}
 
-	for(var i = this.children.length - 1; i >= 0; i--) {
-		var obj = this.children[i];
+Board.prototype.putMines = function(nMine){
+	//爆弾無し
+	if(nMine <= 0){return}
 
-		//オブジェクトを移動させる
-		this.moveObject(obj, deltaTime);
+	//全部爆弾
+	var nBlock = this.sizeX * this.sizeY;
+	if(nMine >= nBlock){
+		for(var i = 0; i < nBlock; i++){
+			this.children[i].isMine = true;
+		}
+		return;
+	}
+/*
+	//ランダムな位置に爆弾生成
+	var count = 0;
+	while(count < nMine){
+		var x = parseInt(Math.random() * this.sizeX);
+		var y = parseInt(Math.random() * this.sizeY);
+		var block = this.getBlock(x,y);
+		if(block === undefined){continue};
+		if(block.isMine = false){
+			block.isMine = true;
+			count++;
+			//incliment around block`s number
+			for(var i = -1; i <= 1; i++){
+				for(var j = -1; j <= 1; j++){
+					if(i == 0 && j == 0){continue;}//me
+					block = getBlock(x+j, y+i);
+					if(block === undefined){continue;}
+					block.number++;
+				}
+			}
+		}
+	}
+*/
+}
 
-		//画面外まで行ったら消す
-		if (obj.y > this.data.height) {
-			this.releaseObject(obj);
+Board.prototype.openBlock = function(objBlock){
+	if(objBlock.isOpen == true || objBlock.isFlag == true){return;}
+	objBlock.isOpen = true;
+	if(objBlock.isMine == true){
+		objBlock.sprite.texture = this.$.resources.imgaes['number' + 9];
+		this.emit('mine');
+		return;
+	}
+	objBlock.sprite.texture = this.$.resources.images['number' + objBlock.number];
+
+	//０番処理
+	if(objBlock.number == 0){
+		//周囲も開ける
+		for(var y = -1; y <= 1; y++){
+			for(var x = -1; x <= 1; x++){
+				if(x == 0 && y == 0){continue;}//me
+				var block = this.getBlock(objBlock.posX + x, objBlock.posY + y);
+				if(block === undefined){continue;}
+				this.openBlock(block);
+			}
 		}
 	}
 }
 
-ObjectContainer.prototype.moveObject = function(obj, deltaTime){
-	obj.y += obj.moveValue * deltaTime;
+Board.prototype.create = function(X,Y){
+	if(X < 0 || Y < 0){return;}
+
+	this.sizeX = X;
+	this.sizeY = Y;
+
+	//生成済みなら消す
+	if(this.children.length > 0){
+		this.release();
+	}
+
+	for(var y = 0; y < this.sizeY; y++){
+		for(var x = 0; x < this.sizeX; x++){
+			//ブロック生成
+			var block = this.addChild(new Block(x, y, this.$.resources.images.block_closed));
+			var self = this;
+			block.on('open', function(block){
+				self.openBlock(block);
+			});
+		}
+	}
+
 }
 
-function Obj(point, texture, container) {
-	Pixim.Container.call(this);
-
-	this.moveValue = this.point = point;
-
-	var sprite = this.addChild(new PIXI.Sprite(texture));
-
-	sprite.width = 50;
-	sprite.height = 50;
-
-	this.on('pointerdown', function() {
-		container.emit('selectObj', this, this.point);
-	});
-
-	this.interactive = true;
-}
-
-Obj.prototype = Object.create(Pixim.Container.prototype);
-
-ObjectContainer.prototype.createObject = function() {
-	var kind = parseInt(Math.random() * 10);
-
-	var obj = this.addChild(new Obj(kind + 1, this.data.resources.images['number' + kind], this));
-
-	obj.x = Math.random() * (this.data.width - obj.width);
-	obj.y = -obj.height;
-}
-
-ObjectContainer.prototype.selectObj = function(obj, point) {
-	//this.root.addScore(point);
-	this.root.emit('addScore', point);
-	this.releaseObject(obj);
-}
-
-ObjectContainer.prototype.releaseObject = function(obj) {
-	this.removeChild(obj);
-}
-
-ObjectContainer.prototype.end = function() {
+Board.prototype.release = function(){
 	this.removeChildren();
 }
 
-var Header = function($){
-	Pixim.Container.call(this);
+Board.prototype.getBlock = function(X,Y){
+	if(X < 0 || X >= this.sizeX || Y < 0 || Y >= this.sizeY){
+		return undefined;
+	}
+	else if(this.children.length <= 0){
+		return undefined;
+	}
+	else{
+		return this.children[this.sizeX * Y + X];
+	}
 
-	this.data = $;
-
-	this.textTime = this.addChild(new PIXI.Text('TIME',{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'}));
-	this.textTime.x = $.width / 2;
-	this.textTime.anchor.x = 0.5;
-
-	this.textScore = this.addChild(new PIXI.Text('SCORE',{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'}));
-	this.textScore.x = $.width / 4 * 3;
-	this.textScore.anchor.x = 0.5;
-}
-Header.prototype = Object.create(Pixim.Container.prototype);
-
-Header.prototype.updateTime = function(time) {
-	this.textTime.text = "TIME : " + time;
 }
 
-Header.prototype.updateScore = function(score) {
-	this.textScore.text = "SCORE : " + score;
-}
-
-Header.prototype.viewResult = function(score) {
-	//時間表示
-	this.textTime.text = "TIME IS OUT";
-
-	//得点表示
-	this.textScore.text = "SCORE : " + score;
-	this.textScore.x = this.data.width / 2;
-	this.textScore.y = this.data.height / 2;
-	this.textScore.anchor.x = 0.5;
-	this.textScore.anchor.y = 0.5;
-}
 
 var Root = function($){
 	Pixim.Container.call(this);
 
-	this.initialTime = InitialTime; //初期時間
-	this.bufTime = 0; //経過時間保存用
+	this.bufTime = 0;
+	this.nowTime = 0;
 
-	this.score = 0; //得点
-
-	this.objectContainer = this.addChild(new $.lib.objectContainer($, this));
+	this.isActive = true;//ゲーム進行フラグ
 
 	this.header = this.addChild(new $.lib.header($));
 
-	this.header.updateScore(this.score);
+	this.board = this.addChild(new $.lib.board($));
+	var self = this;
+	this.board.on('mine', function(){self.isActive = false;});
+	this.board.create(10,10);
+	this.board.initialize(10);
 
-	this.task.on('anim', this.gameloop);
-
-	this.on('addScore', function(score){
-		this.addScore(score);
-	});
+	this.task.on('anim',function(e){this.gameloop(e)});
 }
 
 Root.prototype = Object.create(Pixim.Container.prototype);
 
 Root.prototype.gameloop = function(e){
-	this.bufTime += e.delta / 60;
-	var passedtime = parseInt(this.initialTime - this.bufTime + 1);
+	if(this.isActive){
+		this.bufTime += e.delta;
+		if(this.bufTime >= 60){
+			this.nowTime += 1;
+			this.bufTime -= 60;
+		}
 
-	if(passedtime > 0){
-		//時間内処理
-		this.objectContainer.update(e.delta);
-		this.header.updateTime(passedtime);
+		this.header.updateTime(this.nowTime);
 	}
 	else{
-		//時間外処理
-		this.objectContainer.end();
-
-		this.header.viewResult(this.score);
-
+		//ゲーム終了
 		this.task.clear('anim');
 	}
 }
 
-Root.prototype.addScore = function(score) {
-	this.score += score;
-	this.header.updateScore(this.score);
-}
 
-//--------------------------------
-
-
-//initialization
-//--------------------------------
 
 //Create content
 //Pixim.Content.create('testgame');
@@ -199,22 +239,23 @@ content.defineImages({
 	number7: 'numbers/7.png',
 	number8: 'numbers/8.png',
 	number9: 'numbers/9.png',
+	block_opened: 'blocks/block_opened.png',
+	block_closed: 'blocks/block_closed.png',
+	block_mine: 'blocks/block_mine.png',
 });
 
 content.defineLibraries({
 	root: Root,
-	objectContainer: ObjectContainer,
 	header: Header,
+	board: Board,
 });
 
 
 //Create application
 var app = new Pixim.Application({
-	width: 600,
-	height: 600
+	width: content._piximData.config.width,
+	height: content._piximData.config.height
 });
-
-//--------------------------------
 
 
 //Attach content to application and run application
